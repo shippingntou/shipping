c53d032fb53e0e15fa0ef82a9e98511d322eeaf6
yuyutw123 <yuyutw123@gmail.com>
2022-10-25 00:46:13 +0800

    data all on

--- scripts/main.js
+++ scripts/main.js
@@ -8,16 +8,15 @@ function listBtn() {
   }
 }
 
-// function drop_display() {
-//   let sel_list = document.getElementById("select_item");
-//   if (sel_list.val === 1)
-// }
-
-// var selection = document.getElementById("selec_item");
-
-// selection.onchange = function(event){
-//   var rc = event.target.options[event.target.selectedIndex].val;
-//   var clnc = event.target.options[event.target.selectedIndex].dataset.clnc;
-//   console.log("rc: " + rc);
-//   console.log("clnc: " + clnc);
-// };
+function select() {
+  let selection = document.getElementById("select_item");
+  let visit1 = document.getElementById("visit1");
+  let visit2 = document.getElementById("visit2");
+  visit1.style.display = "none";
+  visit2.style.display = "none";
+  if (selection.selectedIndex === 1) {
+    visit1.style.display = "block";
+  } else if (selection.selectedIndex === 2) {
+    visit2.style.display = "block";
+  }
+}
